30508f9d6b68a239c26bb0a79e893f4431b2e1c8
amiralimiri <amiralimiri3100@gmail.com>
2025-10-04 11:28:30 +0330

    update charities files

--- charities/validators.py
+++ charities/validators.py
@@ -1,7 +1,15 @@
 from django.core import validators
 from django.utils.deconstruct import deconstructible
-
+from typing import Callable
 
 @deconstructible
 class RegNumberValidator(validators.RegexValidator):
-    pass
+    regex = r'^\d{10}$'
+    message = (
+        'Enter a valid registration number. This value may contain numbers only, '
+        'and must be exactly 10 digits'
+    )
+    flags = 0
+
+
+reg_number_validator: Callable[[str], None] = RegNumberValidator()
